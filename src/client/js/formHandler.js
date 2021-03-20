362368f227d5ee65a9bfbd49bfff6b9839c6ceb8
Isidro Lopez <isidrolopez@Isidros-MacBook-Air.local>
2021-03-20 00:44:04 -0500

    successfully working with an external API on my exercise

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,6 +1,6 @@
 function handleSubmit(event) {
     event.preventDefault()
-
+    
     // check what text was put into the form field
     let formText = document.getElementById('name').value
 
@@ -14,6 +14,25 @@ function handleSubmit(event) {
     .then(function(data) {
         document.getElementById('results').innerHTML = data.message
     })
+
+    retrieveData();
+
 }
+
+//retrieve all the data
+const retrieveData = async () =>{ 
+  let url = 'https://api.openweathermap.org/data/2.5/weather?q=quito&appid=ea3ab8e55606cf6bd6983f2eaf2387aa';
+  const request = await fetch(url);
+  try {
+  // Transform into JSON
+  const allData = await request.json()
+  console.log(allData);
+  document.getElementById('results').innerHTML = allData.id
+  return allData;
+  }
+  catch(error) {
+    console.log("error", error);
+  }
+};
 
 export { handleSubmit }
